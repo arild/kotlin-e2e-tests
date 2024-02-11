9c9e1cd32bcaa31f36169b3d007bf386039c9c40
Arild Nilsen <arild@jpro.no>
2024-02-11 12:16:01 +0100

    Upgrade dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.4"
-    id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.spring") version "1.9.10"
-    kotlin("plugin.jpa") version "1.9.10"
-    kotlin("plugin.noarg") version "1.9.10"
-    kotlin("plugin.allopen") version "1.9.10"
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
+    kotlin("plugin.noarg") version "1.9.22"
+    kotlin("plugin.allopen") version "1.9.22"
 }
 
 group = "com.example"
@@ -23,17 +23,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.kafka:spring-kafka")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.postgresql:postgresql:42.3.8")
+    implementation("org.postgresql:postgresql:42.7.1")
     implementation("org.liquibase:liquibase-core")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers:3.2.0-M3")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers:3.2.2")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("org.testcontainers:kafka")
     testImplementation("org.springframework.kafka:spring-kafka-test")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.7.2")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
